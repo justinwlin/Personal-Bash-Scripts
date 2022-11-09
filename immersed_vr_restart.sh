638860b0b0fdcef9e0b36af08338a8a48b69eae9
Justin Lin <justinwlin@gmail.com>
2022-11-09 00:03:24 -0500

    Update immersed_vr_restart.sh

--- immersed_vr_restart.sh
+++ immersed_vr_restart.sh
@@ -12,13 +12,13 @@ then
     echo "Running - Restarting"
     # Run Firefox browser application launch application
     killall Immersed
-    sleep 3
+    # Establish wire connection. I found this doesn't work though unless u the bash script through terminal, so I set up alfred to run this in terminal then run this bash script. 
     adb devices && adb reverse tcp:21000 tcp:21000 && adb reverse --list
     open /Applications/Immersed.app
 
 else
     echo "Not running - starting up"
-    # Run Immersed application
     adb devices && adb reverse tcp:21000 tcp:21000 && adb reverse --list
+    # Run Immersed application
     open /Applications/Immersed.app
 fi
